c46f43ec23c18a1f2569ee4522b27b68970ce806
Pr-DuyTam <prduytam@gmail.com>
2025-02-09 11:29:13 +0700

    a

--- webbanhang/webbanhang/settings.py
+++ webbanhang/webbanhang/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +125,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/images/' # Đường dẫn khi truy cập ảnh
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/static/images') # Tất cả ảnh tải lên sẽ nằm trong đây
